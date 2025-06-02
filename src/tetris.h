174599ac0b0f1ddf73f2873f0fca99a78872aaea
conor <conormartindale@gmail.com>
2025-06-02 20:31:32 +1000

    Planned out header file in more detail

--- src/tetris.h
+++ src/tetris.h
@@ -2,60 +2,71 @@
 
 #pragma once
 
-namespace tetrisBackend {
+enum class Cell {
+    empty,
+    lightBlue,
+    blue,
+    orange,
+    yellow,
+    green,
+    red,
+    purple
+};
 
-    enum class PlayFieldSquare {
-        empty,
-        lightBlue,
-        blue,
-        orange,
-        yellow,
-        green,
-        red,
-        purple
-    };
+class Rectangle {
+    public:
+        int getWidth();
+        int getHeight();
+        Cell getCell(int x, int y);
+};
 
-    class Rectangle {
-        public:
-            int getWidth();
-            int getHeight();
-            PlayFieldSquare getPlayFieldSquare(int x, int y);
-    };
+class Square : public Rectangle {
+};
 
-    class Square : public Rectangle {
-    };
+class Tetromino {
+    public:
+        std::span<Square> getRotations();
+    
+    private:
+        Tetromino(std::span<Square> rotations);
+};
 
-    class Tetromino {
-        public:
-            Square getShape();
-            Square rotateClockwise();
-            Square rotateAntiClockwise();
-    };
+class ActiveTetromino {
+    public:
+        Tetromino& getTetromino();
+        int getRotationIndex();
+        int getXCoordinate();
+        int getYCoordinate();
+};
 
-    class ActiveTetromino {
-        public:
-            Tetromino tetromino();
-            int getXCoordinate();
-            int getYCoordinate();
-    };
+class Tetris {
+    public:
+        Tetris(int width, int height, int initialDifficulty = 0, int nextTetrominoListSize = 3);
+        Tetris(): Tetris(10, 16, 0, 3) {}
+        ~Tetris();
 
-    class Tetris {
-        public:
-            Tetris(int width, int height, int initialDifficulty = 0, int nextTetrominoListSize = 3);
-            Tetris(): Tetris(10, 16, 0, 3) {}
-            ~Tetris();
+        int getScore();
+        int getDifficulty();
 
-            int getScore();
-            int getDifficulty();
+        bool hasSavedPiece();
+        Tetromino& getSavedPiece();
+        ActiveTetromino getActiveTetromino();
 
-            bool hasSavedPiece();
-            Tetromino getSavedPiece();
+        Rectangle getPlayField();
 
-            Rectangle getPlayField();
+        // Defaults to incrementing by 1, but can increase or decrease level by arbitrary amount
+        void updateDifficulty(int increment = 1);
 
-            // Defaults to incrementing by 1, but can increase or decrease level by arbitrary amount
-            void updateDifficulty(int increment = 1);
-            
-            
-    };
+        // Game moves. Returns true if successful
+        bool moveLeft();
+        bool moveRight();
+        bool moveDown();
+        bool moveToBottom();
+        bool rotateClockwise();
+        bool rotateAnticlockwise();
+        bool place();
+};
+
+int main() {
+    Tetromino thing(std::span<Square>());
 }
